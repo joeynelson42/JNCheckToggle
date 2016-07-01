d19e11534aa46c7bbe7a7710d3941ed918aa6ec7
Joey Nelson <joey.nelson@glassdoor.com>
2016-07-01 12:53:53 -0700

    Initial Commit

---
+++ spinningCheckmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spinningCheckmark
+//
+//  Created by Joey Nelson on 7/1/16.
+//  Copyright © 2016 Joey Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
